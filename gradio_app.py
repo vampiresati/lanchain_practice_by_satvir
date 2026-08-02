84f269dc269d8ff78379217427e32dbdeace2811
Satvir <sssatvir12@gmail.com>
2026-08-02 17:10:12 +0530

    Add Gradio app for image-based recipe suggestion
    Introduced a Gradio-based interface that identifies ingredients from an uploaded image and suggests three recipes based on them. Utilizes a chat model initialized with a system prompt to process and respond with recipe suggestions.

---
+++ gradio_app.py
@@ -0,0 +1,39 @@
+import gradio as gr
+import base64
+import mimetypes
+from dotenv import load_dotenv
+from langchain.chat_models import init_chat_model
+
+load_dotenv()
+
+# Initialize the model
+model = init_chat_model("gemini-3.1-flash-lite", model_provider="google_genai")
+system_prompt = """
+"You are a helpful chef. Identify the main ingredients. Suggest 3 recipes based those ingredients."
+"""
+
+def identify_ingredients(image_path):
+    mime_type, _ = mimetypes.guess_type(image_path)
+
+    with open(image_path, 'rb') as file:
+        raw_binary = file.read()
+        base64_bytes = base64.b64encode(raw_binary)
+        image_base64 = base64_bytes.decode("utf-8")
+
+    # Create message
+    message = [{"role": "system", "content": system_prompt},
+               {"role": "user", "content": [{"type": "image", "base64": image_base64, "mime_type": mime_type}]}]
+
+    response = model.invoke(message)
+    return response.text
+
+demo = gr.Interface(
+    fn=identify_ingredients,
+    inputs=gr.Image(type="filepath"),
+    outputs=gr.Markdown(),
+    title='Recipe Generator',
+    description="Upload an image with ingredients",
+    flagging_mode="never"
+ )
+
+demo.launch(debug=True)
